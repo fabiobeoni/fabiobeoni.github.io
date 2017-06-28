e82ed2e60355ce0036471c1d51421deb138c899e
fbfb79 <reg@beoni.net>
2017-06-28 09:53:31 +0200

    Adapt course showcase with Interactive Map Places component

---
+++ adapt/libraries/Utils.js
@@ -0,0 +1,17 @@
+define(function () {
+    return {
+        s4:function() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        },
+        guid:function() {
+            return this.s4() +
+                this.s4() + '-' +
+                this.s4() + '-' +
+                this.s4() + '-' +
+                this.s4() + '-' +
+                this.s4()
+        }
+    };
+});
